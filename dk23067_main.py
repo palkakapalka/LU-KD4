fb3408c31c383f172705f5826ffb65c9dff314af
palkakapalka <kogan_daniel@inbox.lv>
2024-12-21 14:43:33 +0200

    plusx added

--- dk23067_main.py
+++ dk23067_main.py
@@ -1,8 +1,9 @@
 # This is a simple calculation program
 # created to demonstrate version control.
-from libadd import *
-from libmult import *
+from dk23067_libadd import *
+from dk23067_libmult import *
 a = 13
 b = 5
 print(plus(a,b))
+print(plusx(a,b,2))
 print(mult(a,b))
